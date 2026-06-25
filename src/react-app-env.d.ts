4f40126b9b64b085dabf3955f5b934d1875cd45c
hiroshin <hiroshin@hotmail.com>
2026-06-25 22:35:23 +0900

    update on 2026-06-25 from home pc

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="react-scripts" />
+
+// Allow side-effect CSS imports (e.g. import './App.css')
+declare module '*.css' {
+  const styles: { readonly [className: string]: string };
+  export default styles;
+}
